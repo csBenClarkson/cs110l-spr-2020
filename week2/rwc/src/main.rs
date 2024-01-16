fbdb76b6016c749975afc8e52b867ce81640c1c2
BenClarkson <benclarkson@163.com>
2024-01-16 15:06:34 +0800

    finish week 2 rwc.

--- week2/rwc/src/main.rs
+++ week2/rwc/src/main.rs
@@ -1,4 +1,5 @@
-use std::env;
+use std::{env, fs};
+use std::io::{BufRead, BufReader};
 use std::process;
 
 fn main() {
@@ -9,4 +10,25 @@ fn main() {
     }
     let filename = &args[1];
     // Your code here :)
+    let file = fs::File::open(filename)
+        .expect(&*format!("open file {} fails", filename));
+
+    let mut chars: usize = 0;
+    let mut words: usize = 0;
+    let mut lines: usize = 0;
+
+    let mut buf = String::new();
+    let mut buf_reader = BufReader::new(file);
+    while let Ok(code) = buf_reader.read_line(&mut buf) {
+        if code == 0 { break; }
+        chars += buf.len();
+        words += buf.split_whitespace().count();
+        lines += 1;
+        buf.clear();
+    }
+
+    println!("file {}", filename);
+    println!("characters: {}", chars);
+    println!("words: {}", words);
+    println!("lines: {}", lines);
 }
