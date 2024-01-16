43d010f67c078c874145509a77b9985d88a8cd59
BenClarkson <benclarkson@163.com>
2024-01-16 16:11:54 +0800

    finish Milestone 1 & 2.

--- week3/inspect-fds/src/main.rs
+++ week3/inspect-fds/src/main.rs
@@ -1,4 +1,5 @@
 use std::env;
+use nix::libc::printf;
 
 mod open_file;
 mod process;
@@ -10,11 +11,17 @@ fn main() {
         println!("Usage: {} <name or pid of target>", args[0]);
         std::process::exit(1);
     }
-    #[allow(unused)] // TODO: delete this line for Milestone 1
-    let target = &args[1];
 
-    // TODO: Milestone 1: Get the target Process using psutils::get_target()
-    unimplemented!();
+    let target = &args[1];
+    if let Some(proc) = ps_utils::get_target(target)
+        .expect("Error getting target") {
+        println!("Found pid {}", proc.pid);
+        proc.print();
+    }
+    else {
+        println!("Target '{}' did not match any running PIDs or executables", target);
+        std::process::exit(1);
+    }
 }
 
 #[cfg(test)]
