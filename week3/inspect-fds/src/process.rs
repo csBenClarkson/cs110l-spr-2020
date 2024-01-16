c0b0693142c82f0b30cf6c1694606161006badf0
BenClarkson <benclarkson@163.com>
2024-01-16 20:47:42 +0800

    finish Milestone 4.

--- week3/inspect-fds/src/process.rs
+++ week3/inspect-fds/src/process.rs
@@ -33,7 +33,6 @@ impl Process {
     /// This function returns a list of (fdnumber, OpenFile) tuples, if file descriptor
     /// information is available (it returns None otherwise). The information is commonly
     /// unavailable if the process has already exited.
-    #[allow(unused)] // TODO: delete this line for Milestone 4
     pub fn list_open_files(&self) -> Option<Vec<(usize, OpenFile)>> {
         let mut open_files = vec![];
         for fd in self.list_fds()? {
@@ -43,10 +42,25 @@ impl Process {
     }
 
     pub fn print(&self) {
-        println!("===== \"{}\" (pid {}, ppid {}) =====", self.command, self.pid, self.ppid);
-        if let Some(fds) = self.list_fds() {
-            fds.iter().for_each(|fd| { print!("{} ", fd) });
-            println!();
+        println!("========== \"{}\" (pid {}, ppid {}) ==========", self.command, self.pid, self.ppid);
+        match self.list_open_files() {
+            None => println!(
+                "Warning: could not inspect file descriptors for this process! \
+            It might have exited just as we were about to look at its fd table, \
+            or it might have exited a while ago and is waiting for the parent \
+            to reap it."
+            ),
+            Some(open_files) => {
+                for (fd, file) in open_files {
+                    println!(
+                        "{:<4} {:<15} cursor: {:<4} {}",
+                        fd,
+                        format!("({})", file.access_mode),
+                        file.cursor,
+                        file.colorized_name(),
+                    );
+                }
+            }
         }
     }
 }
