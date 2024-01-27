3bd8020875f4ce95100bc9634d37a747e17c7500
BenClarkson <benclarkson@163.com>
2024-01-27 16:08:09 +0800

    deet milestone 5: add breakpoint command. Now program can stop at a particular address. Finish milestone 5.

--- proj-1/deet/src/inferior.rs
+++ proj-1/deet/src/inferior.rs
@@ -1,9 +1,11 @@
+use std::mem::size_of;
 use std::os::unix::process::CommandExt;
 use nix::sys::ptrace;
 use nix::sys::signal;
 use nix::sys::wait::{waitpid, WaitPidFlag, WaitStatus};
 use nix::unistd::{Pid, ResGid};
 use std::process::{Child, Command};
+use libc::select;
 use nix::sys::signal::Signal;
 use crate::dwarf_data::{DwarfData, Line};
 
@@ -33,11 +35,30 @@ pub struct Inferior {
     child: Child,
 }
 
+fn align_addr_to_word(addr: usize) -> usize {
+    addr & (-(size_of::<usize>() as isize) as usize)
+}
+
 impl Inferior {
+    fn write_byte(pid: Pid, addr: usize, val: u8) -> Result<u8, nix::Error> {
+        let aligned_addr = align_addr_to_word(addr);
+        let byte_offset = addr - aligned_addr;
+        let word = ptrace::read(pid, aligned_addr as ptrace::AddressType)? as u64;
+        let orig_byte = (word >> 8 * byte_offset) & 0xff;
+        let masked_word = word & !(0xff << 8 * byte_offset);
+        let updated_word = masked_word | ((val as u64) << 8 * byte_offset);
+        unsafe {
+            ptrace::write(
+                pid,
+                aligned_addr as ptrace::AddressType,
+                updated_word as *mut std::ffi::c_void,
+            )?;
+        }
+        Ok(orig_byte as u8)
+    }
     /// Attempts to start a new inferior process. Returns Some(Inferior) if successful, or None if
     /// an error is encountered.
-    pub fn new(target: &str, args: &Vec<String>) -> Option<Inferior> {
-        // TODO: implement me!
+    pub fn new(target: &str, args: &Vec<String>, breakpoints: &Vec<usize>) -> Option<Inferior> {
         let mut cmd = Command::new(target);
         cmd.args(args);
         unsafe {
@@ -46,7 +67,9 @@ impl Inferior {
         let child = cmd.spawn().ok()?;
         let inferior = Inferior { child };
         let stat = inferior.wait(None).ok()?;
-        if let Status::Stopped(Signal::SIGTRAP, _) = stat {}
+        if let Status::Stopped(Signal::SIGTRAP, _) = stat {
+            breakpoints.iter().for_each(|&x| { Inferior::write_byte(inferior.pid(), x, 0xccu8).ok(); })
+        }
         else { return None }
         Some(inferior)
     }
